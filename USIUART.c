141e9dbcb8c3983e4cf24c44807c2925fac34bb9
Florian Menne <florianmenne@t-online.de>
2016-12-07 13:00:30 +0100

    Improved configuration mechanism. Added OCR0A overflow check during compile time. Added license to all source files.

--- USIUART.c
+++ USIUART.c
@@ -1,9 +1,18 @@
-/*
- * usi_usart.c
- *
- *  Created on: 01.12.2016
- *      Author: flo
- */
+/*  Copyright (C) 2016  Florian Menne
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 
 #include "USIUART.h"
@@ -11,16 +20,33 @@
 #include <stdlib.h>
 #include <string.h>
 #include <avr/io.h>
-#include <avr/iotn85.h>
+//#include <avr/iotn85.h>
 #include <avr/interrupt.h>
 
 #include "bitReverse.h"
 
-#define F_TIMER 1000000
-#define BAUD 9600
-#define TIMER_TICK ((float) F_TIMER/BAUD)
+#ifndef F_CPU
+#define F_CPU 8000000UL
+#warning USIUART.c: F_CPU not defined defaulting to 8000000 Hz
+#endif
 
+#ifndef TIMER_DIVIDER
+#define TIMER_DIVIDER 8
+#endif
+
+#ifndef BAUD
+#define BAUD 9600
+#endif
+
+#ifndef RXBUFFERSIZE
 #define RXBUFFERSIZE 8
+#endif
+
+#if 255 < (F_CPU/TIMER_DIVIDER)/BAUD + ((F_CPU/TIMER_DIVIDER)/BAUD)/2
+#error USIUART.c: Required ticks overflows OCR0A, try increasing TIMER_DIVIDER
+#endif
+
+#define TIMER_TICK ((float) (F_CPU/TIMER_DIVIDER)/BAUD)
 
 enum
 {
@@ -51,7 +77,19 @@ void usiuart_init()
 	 * Set Timer settings
 	 */
 	TCCR0A |= _BV(WGM01);
+#if	TIMER_DIVIDER == 1
+	TCCR0B |= _BV(CS00);
+#elif TIMER_DIVIDER == 8
 	TCCR0B |= _BV(CS01);
+#elif TIMER_DIVIDER == 64
+	TCCR0B |= _BV(CS01) | _BV(CS00);
+#elif TIMER_DIVIDER == 256
+	TCCR0B |= _BV(CS02);
+#elif TIMER_DIVIDER == 1024
+	TCCR0B |= _BV(CS02) | _BV(CS00);
+#else
+#error USIUART.c: Unsupported timer divider
+#endif
 
 	/*
 	 * Enable PCINT0
