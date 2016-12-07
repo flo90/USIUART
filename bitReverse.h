12aac4686cb1a28d634eaea75ba193e360e1ddc4
Florian Menne <flo90dev@gmail.com>
2016-12-07 13:00:30 +0100

    Improved configuration mechanism. Added OCR0A overflow check during compile time. Added license to all source files.

--- bitReverse.h
+++ bitReverse.h
@@ -1,12 +1,21 @@
-/*
- * bitSwap.h
- *
- *  Created on: 01.12.2016
- *      Author: flo
- */
+/*  Copyright (C) 2016  Florian Menne
 
-#ifndef SRC_BITSWAP_H_
-#define SRC_BITSWAP_H_
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef BITSWAP_H_
+#define BITSWAP_H_
 
 #include <stdint.h>
 
@@ -17,4 +26,4 @@
  */
 uint8_t bitReverse(uint8_t byte);
 
-#endif /* SRC_BITSWAP_H_ */
+#endif /* BITSWAP_H_ */
